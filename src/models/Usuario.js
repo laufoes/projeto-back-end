d2fa625ff2a4e080c51aaf515e70deff2a18c04f
laufoes <foeslaura@gmail.com>
2022-09-06 17:08:26 -0300

    usuarioSchema criado

---
+++ src/models/Usuario.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const usuarioSchema = new mongoose.Schema(
+    {
+        id: { type: String },
+        name: { type: String, required: true },
+        cpf: { type: String, required: true},
+        birthDate: { type: Date, required: true},
+        email: { type: String, required: true },
+        password: { type: String, required: true },
+        address: { type: String, required: true },
+        number: { type: String, required: true },
+        complement: { type: String, required: true },
+        city: { type: String, required: true },
+        state: { type: String, required: true },
+        country: { type: String, required: true },
+        zipCode: { type: Number, required: true }
+    }
+);
+
+const usuarios = mongoose.model('usuarios', usuarioSchema);
+
+export default usuarios;

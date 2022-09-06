c71ac3ab6a0dba1ed0e69c6c68aa16e7ed9f64ce
laufoes <foeslaura@gmail.com>
2022-09-06 15:30:59 -0300

    Refatoração de server.js para que agora o app.js passe a lidar com as rotas, utilizando express

--- server.js
+++ server.js
@@ -1,17 +1,6 @@
-const http = require("http");
-const port = 3000;
+import app from './src/app.js';
+const port = process.env.PORT || 3000;
 
-const rotas = {
-    "/": 'Aplicativo back-end',
-    "/api/v1/users": 'Mostra users',
-    "/api/v1/users/:id": 'Mostra por id',
-}
-
-const server = http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end(rotas[req.url]);
-})
-
-server.listen(port, () => {
+app.listen(port, () => {
     console.log(`Servidor escutando em http://localhost:${port}`)
 })
---
+++ src/app.js
@@ -0,0 +1,19 @@
+import express from 'express';
+
+const app = express();
+
+const users = [
+    {
+        id: 1, "name": "João Silva", "cpf": "908.415.400-20", "birthDate": "01/01/2000", "email": "joao.silva@compasso.com", "password": "swordfish", "address": "street A", "number": "A25", "complement": "house", "city": "São Paulo", "state": "SP", "country": "Brasil", "zipCode": "93950-000"
+    }
+]
+
+app.get("/", (req, res) => {
+    res.status(200).send('Página inicial do projeto back-end');
+})
+
+app.get("/api/v1/users", (req, res) => {
+    res.status(200).json(users)
+})
+
+export default app

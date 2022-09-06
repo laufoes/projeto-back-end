3187311906e348c3aaf71eb835160912c6370a3e
laufoes <foeslaura@gmail.com>
2022-09-06 17:50:53 -0300

    Passando o Model como parâmetro para as operações do banco

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import db from './config/dbConnect.js';
+import usuarios from './models/Usuario.js';
 
 db.on("error", console.log.bind(console, "Erro de conexão."));
 db.once("open", () => {
@@ -10,22 +11,16 @@ const app = express();
 
 app.use(express.json());
 
-const users = [
-    {
-        id: 1, "name": "João Silva", "cpf": "908.415.400-20", "birthDate": "01/01/2000", "email": "joao.silva@compasso.com", "password": "swordfish", "address": "street A", "number": "A25", "complement": "house", "city": "São Paulo", "state": "SP", "country": "Brasil", "zipCode": "93950-000"
-    }
-]
-
 app.get("/", (req, res) => {
     res.status(200).send('Página inicial do projeto back-end');
 })
 
 app.get("/api/v1/users", (req, res) => {
-    res.status(200).json(users)
+    res.status(200).json(usuarios)
 })
 
 app.post("/api/v1/users", (req, res) => {
-    users.push(req.body);
+    usuarios.push(req.body);
     res.status(201).send('O usuário foi cadastrado com sucesso')
 })
 

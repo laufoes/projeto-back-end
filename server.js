c5dcc26c32878ea2497b1e5f67b3add48ac7abea
laufoes <foeslaura@gmail.com>
2022-09-06 14:59:34 -0300

    Primeiro commit, inicializando o server

---
+++ server.js
@@ -0,0 +1,16 @@
+const http = require("http");
+const port = 3000;
+
+const routes = {
+    "/api/v1/users": 'Mostra users',
+    "/api/v1/users/:id": 'Mostra por id',
+}
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end(rotas[req.url]);
+})
+
+server.listen(port, () => {
+    console.log(`Servidor escutando em http://localhost:${port}`)
+})

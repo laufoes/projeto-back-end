c4d4ce0a9a738c1eb41bdfa949396e69dacb634d
laufoes <foeslaura@gmail.com>
2022-09-06 22:52:34 -0300

    Email regex update

--- src/models/Usuario.js
+++ src/models/Usuario.js
@@ -1,12 +1,17 @@
 import mongoose from "mongoose";
 
+const validaEmail = (email) => {
+    var regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    return regex.test(email)
+}
+
 const usuarioSchema = new mongoose.Schema(
     {
         id: { type: String },
         name: { type: String, required: true },
         cpf: { type: String, required: true, minlength: 11, maxlength: 11 },
         birthDate: { type: Date, required: true, max: '01-01-2004'},
-        email: { type: String, required: true, validate: [/[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/, 'Por favor insira um endereço de e-mail válido.'] },
+        email: { type: String, required: true, validate: [validaEmail, 'Por favor insira um endereço de e-mail válido.'] },
         password: { type: String, required: true, minlength: 6 },
         address: { type: String, required: true },
         number: { type: String, required: true },

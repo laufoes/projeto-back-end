c988397eabda1589ded0f73ed89c8e2a2a930a68
laufoes <foeslaura@gmail.com>
2022-09-06 22:31:23 -0300

    Validação de campos implementada

--- src/models/Usuario.js
+++ src/models/Usuario.js
@@ -1,17 +1,12 @@
 import mongoose from "mongoose";
 
-function validateEmail(email) {
-    var regex = /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/;
-    return regex.test(email);
-};
-
 const usuarioSchema = new mongoose.Schema(
     {
         id: { type: String },
         name: { type: String, required: true },
-        cpf: { type: String, required: true},
-        birthDate: { type: String, required: true},
-        email: { type: String, required: true, validate: [validateEmail, 'Por favor insira um endereço de e-mail válido.'] },
+        cpf: { type: String, required: true, minlength: 11, maxlength: 11 },
+        birthDate: { type: Date, required: true, max: '01-01-2004'},
+        email: { type: String, required: true, validate: [/[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/, 'Por favor insira um endereço de e-mail válido.'] },
         password: { type: String, required: true, minlength: 6 },
         address: { type: String, required: true },
         number: { type: String, required: true },
@@ -19,7 +14,10 @@ const usuarioSchema = new mongoose.Schema(
         city: { type: String, required: true },
         state: { type: String, required: true },
         country: { type: String, required: true },
-        zipCode: { type: Number, required: true }
+        zipCode: { type: Number, required: true, minlength: 8, maxlength: 8 }
+    },
+    {
+        versionKey: false
     }
 );
 
